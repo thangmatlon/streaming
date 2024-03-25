36cc7a14fff949e8ff86064183942f8c872ce812
loybung <loybung2022@gmail.com>
2024-03-25 12:34:44 +0700

    fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv/config");
 const { launcher } = require("@loybung/launcher");
+const keep_alive = require("./server");
 
 const launcherObject = {
   url: "https://loybung-api.vercel.app/project/streaming",
